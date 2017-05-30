7daa0185edec46a2d10e3e21c692aeedacdd96fb
SoumenITMO <soumenbanerjeenik@gmail.com>
2017-05-30 14:07:33 +0300

    vacancy_keyword_extraction

--- src/main/scala/scala_word_count.scala
+++ src/main/scala/scala_word_count.scala
@@ -1,33 +1,30 @@
+//import org.json4s.JsonDSL._
 import org.apache.spark.SparkContext
 import org.json4s._
-//import org.json4s.JsonDSL._
 import org.json4s.jackson.JsonMethods._
 import org.apache.hadoop.conf.Configuration
 import org.bson.BSONObject
 
 object scala_word_count {
-
   def main(args: Array[String])
   {
-    val sc = new SparkContext("local", "Scala Word Count")
+    val sc = new SparkContext("local", "Scala Vacancy Keyword Generator")
     val config = new Configuration()
     //config.set("mongo.input.uri", "mongodb://127.0.0.1:27017/local.test_mongo_spark")
-    config.set("mongo.input.uri", "mongodb://127.0.0.1:27017/local.new_plane_collection")
+    config.set("mongo.input.uri", "mongodb://127.0.0.1:27017/local.new_plane_collection")  // "new_plane_collection" the collection name
     config.set("mongo.output.uri", "mongodb://127.0.0.1:27017/local.output__")
-    val mongoRDD = sc.newAPIHadoopRDD(config, classOf[com.mongodb.hadoop.MongoInputFormat], classOf[Object], classOf[BSONObject])
+    val mongoRDD = sc.newAPIHadoopRDD(config, classOf[com.mongodb.hadoop.MongoInputFormat], classOf[Object],
+      classOf[BSONObject])
     //print("COUNT FROM MONGODB --- > " + mongoRDD.count())
-    val bsonRDD = mongoRDD.map(x=>x._2.get("Plane_17_12_16"))	// Array[(Object, org.bson.BSONObject)] --> Array[org.bson.BSONObject]
+    //val bsonRDD = mongoRDD.map(x=>x._2.get("Plane_17_12_16"))	// Array[(Object, org.bson.BSONObject)] --> Array[org.bson.BSONObject]
+    val bsonRDD = mongoRDD.map(x=>x._2.get("Plane_03_07_16"))	// Array[(Object, org.bson.BSONObject)] --> Array[org.bson.BSONObject]
+
     val jsonStringRDD = bsonRDD.map(x => x.toString)	// org.apache.spark.rdd.RDD[org.bson.BSONObject] --> org.apache.spark.rdd.RDD[String]
-
-    var parsedData = jsonStringRDD.foreach(a=>{
-        val JSON_ = parse(a)
-        var name = for { JString(x) <- JSON_ \\ "number" } yield x  // WHERE "number" is a key of the data document
-        for (k<-name)
-        {
-            println(k)
-        }
+    jsonStringRDD.foreach(rddJson=>{
+        var number_list = for { JString(x) <- parse(rddJson) } yield x  // WHERE "number" is a key of the data document
+        println(number_list)
+        //for (extract_elemtn_ <- number_list)  println(extract_elemtn_)
     })
-
     /*
     val data__rdd = mongoRDD.foreach(a=>{
         val k = JSON.parseFull(a)
@@ -40,7 +37,6 @@ object scala_word_count {
       str = str.toLowerCase().replaceAll("[.,!?\n]", " ")
       str.split(" ")
     }).map(word => (word, 1)).reduceByKey((a, b) => a + b)
-
     val saveRDD = countsRDD.map((tuple) => {
       var bson = new BasicBSONObject()
       bson.put("word", tuple._1)
