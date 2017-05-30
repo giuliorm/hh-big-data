44520dbfbc6c6d0e609d394304ef3b54814c3534
SoumenITMO <soumenbanerjeenik@gmail.com>
2017-05-30 14:53:25 +0300

    keyword_extraction

--- src/main/scala/scala_word_count.scala
+++ src/main/scala/scala_word_count.scala
@@ -11,20 +11,32 @@ object scala_word_count {
     val sc = new SparkContext("local", "Scala Vacancy Keyword Generator")
     val config = new Configuration()
     //config.set("mongo.input.uri", "mongodb://127.0.0.1:27017/local.test_mongo_spark")
-    config.set("mongo.input.uri", "mongodb://127.0.0.1:27017/local.new_plane_collection")  // "new_plane_collection" the collection name
+    config.set("mongo.input.uri", "mongodb://127.0.0.1:27017/local.Plane_03_07_16")  // "new_plane_collection" the collection name
     config.set("mongo.output.uri", "mongodb://127.0.0.1:27017/local.output__")
     val mongoRDD = sc.newAPIHadoopRDD(config, classOf[com.mongodb.hadoop.MongoInputFormat], classOf[Object],
       classOf[BSONObject])
-    //print("COUNT FROM MONGODB --- > " + mongoRDD.count())
-    //val bsonRDD = mongoRDD.map(x=>x._2.get("Plane_17_12_16"))	// Array[(Object, org.bson.BSONObject)] --> Array[org.bson.BSONObject]
-    val bsonRDD = mongoRDD.map(x=>x._2.get("Plane_03_07_16"))	// Array[(Object, org.bson.BSONObject)] --> Array[org.bson.BSONObject]
+    print("NUMNER OF RECORDS FROM  MONGODB --- > \n " + mongoRDD.count())
+    if(mongoRDD.count()!=0)
+    {
+          val bsonRDD = mongoRDD.map(x=>x._2.get("Plane"))	// Array[(Object, org.bson.BSONObject)] --> Array[org.bson.BSONObject]
+          val jsonStringRDD = bsonRDD.map(x => x.toString)
+          jsonStringRDD.foreach(rddJson=> {
+            var values__ = for { JString(x) <- parse(rddJson) \ "number"} yield x
+            for(extract_val <- values__){println(extract_val)}
+            print("-------------------------------------------------------------------------------\n")
+          })
+    }
 
+    // ------------------------------------------- Don't Delete -------------------------------------------------------
+    //val bsonRDD = mongoRDD.map(x=>x._2.get("Plane_03_07_16"))	// Array[(Object, org.bson.BSONObject)] --> Array[org.bson.BSONObject]
+    /*
     val jsonStringRDD = bsonRDD.map(x => x.toString)	// org.apache.spark.rdd.RDD[org.bson.BSONObject] --> org.apache.spark.rdd.RDD[String]
     jsonStringRDD.foreach(rddJson=>{
-        var number_list = for { JString(x) <- parse(rddJson) } yield x  // WHERE "number" is a key of the data document
-        println(number_list)
+         println(rddJson)
+        //var number_list = for { JString(x) <- parse(rddJson) \\ "number" } yield x  // WHERE "number" is a key of the data document
         //for (extract_elemtn_ <- number_list)  println(extract_elemtn_)
-    })
+    })*/
+
     /*
     val data__rdd = mongoRDD.foreach(a=>{
         val k = JSON.parseFull(a)
