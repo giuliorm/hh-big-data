9dc59744a48e4dfb08e668d4675897496a1cd6b3
juriasan <giulio.rm95@gmail.com>
2017-05-31 11:42:11 +0300

    changed vacancies loader script

--- scala-spark/src/main/scala/scala_word_count.scala
+++ scala-spark/src/main/scala/scala_word_count.scala
@@ -1,5 +1,6 @@
 //import org.json4s.JsonDSL._
-import org.apache.spark.SparkContext
+import com.fasterxml.jackson.annotation.JsonValue
+import org.apache.spark.{SparkConf, SparkContext}
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
 import org.apache.hadoop.conf.Configuration
@@ -8,23 +9,47 @@ import org.bson.BSONObject
 object scala_word_count {
   def main(args: Array[String])
   {
-    val sc = new SparkContext("local", "Scala Vacancy Keyword Generator")
+    val conf = new SparkConf().setAppName("hh-scala-spark").setMaster("local").set("spark.executor.memory", "2g")
+    val sc = new SparkContext(conf)
+
     val config = new Configuration()
+    val collectionName = "vacancies-test"
+    val outputCollectionName = "vacancies-test-output"
+    val databaseName = "hh-crawler"
+
     //config.set("mongo.input.uri", "mongodb://127.0.0.1:27017/local.test_mongo_spark")
-    config.set("mongo.input.uri", "mongodb://127.0.0.1:27017/local.Plane_03_07_16")  // "new_plane_collection" the collection name
-    config.set("mongo.output.uri", "mongodb://127.0.0.1:27017/local.output__")
+    config.set("mongo.input.uri", "mongodb://127.0.0.1:27017/" + databaseName + "." + collectionName)  // "new_plane_collection" the collection name
+    config.set("mongo.output.uri", "mongodb://127.0.0.1:27017/" + databaseName + "." + outputCollectionName)
+
+
     val mongoRDD = sc.newAPIHadoopRDD(config, classOf[com.mongodb.hadoop.MongoInputFormat], classOf[Object],
       classOf[BSONObject])
+
     print("NUMNER OF RECORDS FROM  MONGODB --- > \n " + mongoRDD.count())
+
     if(mongoRDD.count()!=0)
     {
-          val bsonRDD = mongoRDD.map(x=>x._2.get("Plane"))	// Array[(Object, org.bson.BSONObject)] --> Array[org.bson.BSONObject]
-          val jsonStringRDD = bsonRDD.map(x => x.toString)
-          jsonStringRDD.foreach(rddJson=> {
+      val jsonStringRDD = mongoRDD.map(x => x._2.get("Plane").toString)	// Array[(Object, org.bson.BSONObject)] --> Array[org.bson.BSONObject]
+      implicit val formats = DefaultFormats
+      val vacanciesTestRepresentation = jsonStringRDD
+            .map(json => {
+              val parsedJson = parse(json)
+              val name = (parsedJson \ "name").extract[String]
+              val description = (parsedJson \ "description").extract[String]
+              val skills = (parsedJson \ "key_skills").extract[Array[String]]
+
+              //remove same words
+              //remove most common words
+              //
+
+            })
+
+
+      /*     jsonStringRDD.foreach(rddJson => {
             var values__ = for { JString(x) <- parse(rddJson) \ "number"} yield x
             for(extract_val <- values__){println(extract_val)}
             print("-------------------------------------------------------------------------------\n")
-          })
+          })*/
     }
 
     // ------------------------------------------- Don't Delete -------------------------------------------------------
