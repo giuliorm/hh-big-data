f2f578f726eebf2f9f981f9d100cad3da8ccd2a1
SoumenITMO <soumenbanerjeenik@gmail.com>
2017-05-31 07:52:44 +0300

    keyword_extraction

--- src/main/scala/keyword_extractor_.scala
+++ src/main/scala/keyword_extractor_.scala
@@ -28,6 +28,36 @@ object keyword_extractor_ {
           println("--------------------------------------------------------- \n ")
         })
       }
+    // ------------------------------------------- Don't Delete -------------------------------------------------------
+    //val bsonRDD = mongoRDD.map(x=>x._2.get("Plane_03_07_16"))	// Array[(Object, org.bson.BSONObject)] --> Array[org.bson.BSONObject]
+    /*
+    val jsonStringRDD = bsonRDD.map(x => x.toString)	// org.apache.spark.rdd.RDD[org.bson.BSONObject] --> org.apache.spark.rdd.RDD[String]
+    jsonStringRDD.foreach(rddJson=>{
+         println(rddJson)
+        //var number_list = for { JString(x) <- parse(rddJson) \\ "number" } yield x  // WHERE "number" is a key of the data document
+        //for (extract_elemtn_ <- number_list)  println(extract_elemtn_)
+    })*/
+
+    /*
+    val data__rdd = mongoRDD.foreach(a=>{
+        val k = JSON.parseFull(a)
+        print(k)
+    })
+    */
+    /*
+    val countsRDD = mongoRDD.flatMap(arg => {
+      var str = arg._2.get("text").toString
+      str = str.toLowerCase().replaceAll("[.,!?\n]", " ")
+      str.split(" ")
+    }).map(word => (word, 1)).reduceByKey((a, b) => a + b)
+    val saveRDD = countsRDD.map((tuple) => {
+      var bson = new BasicBSONObject()
+      bson.put("word", tuple._1)
+      bson.put("count", tuple._2)
+      (null, bson)
+    })
+    saveRDD.saveAsNewAPIHadoopFile("E://spark-count.txt", classOf[Any], classOf[Any], classOf[com.mongodb.hadoop.MongoOutputFormat[Any, Any]], config)
+    */
 
   }
 }
